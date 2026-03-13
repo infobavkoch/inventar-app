d43e3b257dda7e8babed0a3e1d1decd6c76d1b80
Max <infobav.koch@gmail.com>
2026-03-13 13:00:02 +0100

    fix syntax error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from auth import login
 from components.dashboard import dashboard
 
-ith open("style.css") as f:
+with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 st.set_page_config(page_title="Inventarverwaltung", layout="wide")
